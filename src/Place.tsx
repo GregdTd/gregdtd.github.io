1d47551c1c67b3d96003f5e8235ed02620ac3418
GregdTd <gregoire@lalilo.com>
2023-11-26 17:07:00 +0100

    Try to deploy

--- src/Place.tsx
+++ src/Place.tsx
@@ -1,11 +1,7 @@
-
-
-import Kerantre from './assets/kerantre.jpeg'
-
 export const Place: React.FC = () => {
     return (
-        <section id='place'>
-            <img alt='Photo du château de Kerantré' src={Kerantre} width={'100px'}/>
+        <section id="place">
+            <img alt="Photo du château de Kerantré" src={''} width={'100px'} />
         </section>
     )
 }
