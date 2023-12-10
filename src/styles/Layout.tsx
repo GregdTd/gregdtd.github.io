3d7fcf0ea67d0c77afd040f8b59f3be169c3a712
Grégoire de Trogoff <gregoire@lalilo.com>
2023-12-10 20:35:41 +0100

    Update Layout.tsx

--- src/styles/Layout.tsx
+++ src/styles/Layout.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { Pages, PagesTitles } from '../utils/types'
 import { Colors } from './Colors'
 
-export const SectionWrapper: React.FC<{ id: Pages; children?: React.ReactNode; className?: string }> = ({ title, id, children, className }) => {
+export const SectionWrapper: React.FC<{ id: Pages; children?: React.ReactNode; className?: string }> = ({ id, children, className }) => {
     return (
         <Wrapper>
             <Section id={id} className={className}>
